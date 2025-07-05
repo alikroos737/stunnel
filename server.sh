18b6af00b3034cee2934fa756506844546dd650b
alikroos737 <zahed3turk@gmail.com>
2025-07-05 09:53:15 +0330

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# setup_stunnel_server.sh
+# روی سرور مقصد اجرا شود
+
+apt update
+apt install -y stunnel4
+
+cat > /etc/stunnel/stunnel-server.conf <<EOF
+pid = /var/run/stunnel-server.pid
+foreground = yes
+debug = 7
+
+[http]
+accept = 8880
+connect = 127.0.0.1:80
+
+[https]
+accept = 8444
+connect = 127.0.0.1:443
+EOF
+
+# اجازه بده stunnel اجرا بشه
+sed -i 's/ENABLED=0/ENABLED=1/' /etc/default/stunnel4
+
+systemctl restart stunnel4
+
+# اجرا دستی برای تست
+stunnel /etc/stunnel/stunnel-server.conf
